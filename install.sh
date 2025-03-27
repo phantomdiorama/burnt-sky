684ff8045064bad1b8b6c3e8e5d2ae9ca301cca4
phantom diorama <phantomdiorama@gmail.com>
2025-03-27 13:52:33 +1100

    Version 0.1
    ! Version info in footer
    ! Better commented code
    ! Code clean up

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # install.sh
 
-# make script executable
-# and move to local bin
+# assuming ~/.local/bin is in PATH
+# make script executable and move to local bin
 chmod +x hurryasteroid && mv hurryasteroid $HOME/.local/bin/
