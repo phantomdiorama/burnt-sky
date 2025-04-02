2af4c6db59e92cc62d0338ccf21b967f4c740ea6
phantom diorama <phantomdiorama@gmail.com>
2025-04-02 20:30:11 +1100

    Use async

--- synctest.py
+++ synctest.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 # duggee.py
 
-import uuid
 import httpx
+import uuid
 import asyncio
+import feedparser
+#########################
+# sync stuff
+#########################
 
 async def get_async(url):
     async with httpx.AsyncClient() as client:
         return await client.get(url)
 
 urls = []
+blusers = []
 
 with open("users.txt") as file:
     for line in file:
@@ -18,11 +23,19 @@ with open("users.txt") as file:
 
 async def launch():
     resps = await asyncio.gather(*map(get_async, urls))
-    data = [resp.text for resp in resps]
+    data = [resp for resp in resps]
     for html in data:
         print(html)
-        filename = str(uuid.uuid4())
-        with open(filename, "w") as f:
-            f.write(html)
+        blusers.append(html)
+        # filename = str(uuid.uuid4())
+        # with open(dump_path / filename, "w") as f:
+        #     f.write(html)
 
 asyncio.run(launch())
+
+for i in blusers:
+    d = feedparser.parse(i)
+    k = d.feed.title
+    print(k)
+#########################
+#########################
